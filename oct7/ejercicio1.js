1a0fb125413532ad7b35ad9e9e2e225382ddf2cd
ScarlettDuna <arantxa.rg.1991@hotmail.com>
2025-10-07 09:58:10 +0200

    ejercicios de clase del 7 octubre

---
+++ oct7/ejercicio1.js
@@ -0,0 +1,16 @@
+/* Vamos a simular sorteo de lotería. Para ello, solicita un número en el rango del 1 al 10 al usuario. Primero valida que el número introducido
+sea correcto con las funciones estudiadas en este bloque. Después genera un
+número aleatorio. Si los números coinciden el usuario habrá sido agraciado, en caso
+contrario no. */
+let numUsuario = Number(prompt("Introduce un número del 1 al 10"));
+if (isNaN(numUsuario) || numUsuario < 1 || numUsuario > 10) {
+    alert("Número fuera de rango o valor incorrecto")
+} else {
+    let numAleatorio = Math.floor(Math.random() * 11)
+    console.log(numAleatorio)
+    if (numAleatorio === numUsuario) {
+        alert("¡Has ganado!")
+    } else {
+        alert("¡Otra vez será")
+    }
+}
---
+++ oct7/ejercicio2.js
@@ -0,0 +1,16 @@
+/* Crea un script que simule el análisis de precios de productos en una tienda. El usuario debe introducir:
+Un precio base en euros (por prompt). */
+let precio = parseFloat(prompt("Introduce el precio del producto"))
+// Un número con decimales para representar un porcentaje de descuento.
+let descuento = parseFloat(prompt("Introduce el porcentaje de descuento, en decimal"))
+/* Tu script debe:
+ Validar que los dos valores sean números válidos.
+ Convertir los valores a tipo numérico real.
+ Calcular el precio final aplicando el descuento.
+ Mostrar los errores o el resultado final con alert. */
+if (isNaN(precio) || isNaN(descuento) || precio < 0 || descuento < 0) {
+    alert("Datos introducidos no válidos");
+} else {
+    let precio_final = precio - (precio *(descuento / 100))
+    alert(`Precio inicial: ${precio}€ \nDescuento: ${descuento}%. \nEl precio final es ${precio_final}€`)
+}
