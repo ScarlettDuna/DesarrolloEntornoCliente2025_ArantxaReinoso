adbcba88d153e8b5fb14ebfffdf2b42e307dcddd
Arantxa Reinoso <154524564+ScarlettDuna@users.noreply.github.com>
2025-11-26 10:17:14 +0100

    inicio calculadora

---
+++ Unidad3/Machacada-pre-examen/Ejercicios_integrados/2-calculadora.js
@@ -0,0 +1,7 @@
+let numeros = document.querySelector('.numeros')
+let operadores = document.querySelector('.operadores')
+let pantalla = document.querySelector('.pantalla p')
+
+let cifra1 = 0
+let cifra2 = 0
+
