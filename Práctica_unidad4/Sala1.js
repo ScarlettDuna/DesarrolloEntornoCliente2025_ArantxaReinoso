930aff02d49b96de04ec4237bc0f33297653c6f7
ScarlettDuna <arantxa.rg.1991@hotmail.com>
2025-12-08 13:59:22 +0100

    Práctica 4 - Sala 1 completada

--- Práctica_unidad4/Sala1.js
+++ Práctica_unidad4/Sala1.js
@@ -11,37 +11,47 @@ let sala1 = {
         5: numeroAleatorio % 5 === 0 ? "Es divisible por 5" : "No es divisible por 5",
         6: numeroAleatorio % 7 === 0 ? "Es divisible por 7" : "No es divisible por 7"
     },
-    [codigoSecreto]: numeroAleatorio,
     intentosAcceder: 10,
     intentosHechos: 0,
-    validarCodigo(codigoIntroducido) {
-        if (codigoIntroducido === this[codigoSecreto]) {
-            this.bloqueada = false;
-            this.addIntento();
-            return "Correcto";
-        } else {
-            this.addIntento();
-            return "Incorrecto";
-        }
-    },
-    addIntento() {
-        this.intentosHechos ++;
-        document.getElementById("intentosUsados").innerText = this.intentosHechos;
-        if (this.intentosHechos >= this.intentosAcceder) {
-            document.getElementById("inputCodigo").style.display = "none";
-            document.getElementById("probarCodigo").style.display = "none";
-            document.getElementById("zonaPistas").innerText = "Número de intentos agotado. Reinicia la sala para empezar de nuevo."
-        }
-    }
+
 }
 // Clonación del objeto Sala1
 const estadoInicialSala1 = structuredClone(sala1);
 
-
+// Añadimos métodos y symbol al objeto sala1
+sala1[codigoSecreto] = numeroAleatorio;
+sala1.validarCodigo = function (codigoIntroducido) {
+    if (codigoIntroducido === this[codigoSecreto]) {
+        this.bloqueada = false;
+        this.addIntento();
+        return "Correcto";
+    } else {
+        this.addIntento();
+        return "Incorrecto";
+    }
+}
+const fnValidarCodigo = sala1.validarCodigo;
+sala1.addIntento = function () {
+    this.intentosHechos ++;
+    document.getElementById("intentosUsados").innerText = this.intentosHechos;
+    if (this.intentosHechos >= this.intentosAcceder) {
+        document.getElementById("inputCodigo").style.display = "none";
+        document.getElementById("probarCodigo").style.display = "none";
+        document.getElementById("zonaPistas").innerText = "Número de intentos agotado. Reinicia la sala para empezar de nuevo."
+    }
+}
+const fnAddIntento = sala1.addIntento;
 // Comprobar input con códigoSecreto
 document.getElementById("probarCodigo").addEventListener("click", () => {
     const valor = Number(document.getElementById("inputCodigo").value);
     const resultado = sala1.validarCodigo(valor);
+    if (resultado === "Incorrecto") {
+        document.getElementById("resultado").classList.add("error");
+        setTimeout(() => {
+            document.getElementById("resultado").classList.remove("error");
+        }, 300);
+
+    }
     document.getElementById("resultado").innerText = resultado;
 });
 
@@ -50,11 +60,12 @@ let pista = 1;
 const contenedor = document.getElementById("zonaPistas");
 contenedor.innerHTML = "";
 document.getElementById("verPistas").addEventListener("click", () => {
-    if (pista >= 6) {
+    if (pista <= 6) {
         contenedor.innerHTML += `<p>${pista}. ${sala1.pistas[pista]}</p> \n`;
         pista ++;
     } else {
         contenedor.innerHTML += "No hay más pistas";
+        document.getElementById("verPistas").style.display = 'none';
     }
 
 });
@@ -62,7 +73,11 @@ document.getElementById("verPistas").addEventListener("click", () => {
 // Reiniciar Sala
 document.getElementById("reiniciarSala").addEventListener("click", () => {
     // Restaurar estado completo desde la copia
+    let nuevoCodigo = Math.floor(Math.random() * 900) + 100;
     sala1 = structuredClone(estadoInicialSala1);
+    sala1[codigoSecreto] = nuevoCodigo;
+    sala1.validarCodigo = fnValidarCodigo;
+    sala1.addIntento = fnAddIntento;
 
     // Restaurar interfaz
     document.getElementById("intentosUsados").innerText = 0;
