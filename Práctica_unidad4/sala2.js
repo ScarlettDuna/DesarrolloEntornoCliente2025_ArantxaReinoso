e6fb763b3ba9cf39b2154593258c2442447b86f0
ScarlettDuna <arantxa.rg.1991@hotmail.com>
2025-12-16 20:39:13 +0100

    Práctica 4 - Se añade método mostrarEstado, set conbinacionesUsadas y map pistas

--- Práctica_unidad4/sala2.js
+++ Práctica_unidad4/sala2.js
@@ -15,17 +15,57 @@ let sala2 = {
   },
   tiempoLimite: 60, // 60 segundos
   intentos: 0, 
+  pistas: new Map([
+    ["A", "La palanca A parece desgastada"],
+    ["B", "La palanca B no hace ruido"],
+    ["C", "La palanca C vibra ligeramente"]
+  ]),
+  combinacionesUsadas: new Set(), //Cada vez que se pulsa comprobar se guarda la combianción por si el usuario quiere ver qué opciones ha introducido ya
   cambiarPalanca(palanca) {
     this.palancas[palanca] = !this.palancas[palanca]
   },
   comprobarCombinacion() {
     this.intentos ++;
-    for (let palanca in this.combinacionCorrecta) {
-        if (this.palancas[palanca] !== this.combinacionCorrecta[palanca]) {
-        return false;
-        }
+    const clavePalancas = Array.from(
+      Object.entries(this.palancas),
+      ([p, v]) => `${p}:${v}`).join('|');
+    this.combinacionesUsadas.add(clavePalancas)
+    const correcta = Object.keys(this.combinacionCorrecta).every(p => this.palancas[p] === this.combinacionCorrecta[p]);
+    if (correcta) {
+      this.bloqueada = false;
+      return true;
+    } else {
+      return false;
     }
-    this.bloqueada = false;
-    return true;
+  }, 
+  mostrarEstado() {
+    return Object.entries(this.palancas).map(([palanca, estado]) => {
+      return `${palanca}: ${estado ? "ON" : "OFF"}`;
+    }).join('\n')
   }
 }
+let resultado = document.querySelector('.resultado')
+let palancaA = document.querySelector('.palA')
+let palancaB = document.querySelector('.palB')
+let palancaC = document.querySelector('.palC')
+let comprobar = document.querySelector('#comprobar')
+let mostrar = document.querySelector('.mostrar')
+
+palancaA.addEventListener('click', () => {
+  sala2.cambiarPalanca('A');
+})
+palancaB.addEventListener('click', () => {
+  sala2.cambiarPalanca('B');
+})
+palancaC.addEventListener('click', () => {
+  sala2.cambiarPalanca('C');
+})
+comprobar.addEventListener('click', () => {
+  const ok = sala2.comprobarCombinacion();
+  resultado.innerText = ok
+    ? "✔ Combinación correcta"
+    : "✖ Combinación incorrecta";
+})
+mostrar.addEventListener('click', () => {
+  resultado.innerHTML = sala2.mostrarEstado()
+})
