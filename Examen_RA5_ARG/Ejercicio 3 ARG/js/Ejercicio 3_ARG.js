b0adc31afddec070a73d5c232afc6836a9564c8c
Arantxa Reinoso <154524564+ScarlettDuna@users.noreply.github.com>
2026-02-25 13:48:03 +0100

    examen

---
+++ Examen_RA5_ARG/Ejercicio 3 ARG/js/Ejercicio 3_ARG.js
@@ -0,0 +1,37 @@
+﻿let contenedor = document.getElementById("contenedor");
+let arrastrable = document.getElementById("arrastrable");
+let info = document.getElementById("info");
+
+//TODO Elige el tipo de evento y completa el código
+contenedor.addEventListener("mousemove", function(evento) {
+  evento.stopPropagation()
+     // Dimensiones incluyendo padding y borde
+    const ancho = contenedor.offsetWidth;
+    const alto = contenedor.offsetHeight;
+
+    // Para obtener la posición relativa a la ventana    
+    const coordX = evento.pageX;
+    const coordY = evento.pageY;
+
+    // Posición del cursor dentro del contenedor
+    const cursorX = evento.offsetX;
+    const cursorY = evento.offsetY;
+
+    info.textContent = `Dimensiones: ${ancho}px x ${alto}px, 
+      Posición del contenedor: (${Math.round(coordX)}, ${Math.round(coordY)}), 
+      Posición del cursor dentro: (${Math.round(cursorX)}, ${Math.round(cursorY)})`;
+});
+contenedor.addEventListener("mouseout", () => {
+  info.textContent = ``;
+})
+
+//TODO Completar el evento para arrastrar y soltar
+contenedor.addEventListener("dragover", function(evento) {
+  evento.preventDefault();
+});
+
+contenedor.addEventListener("drop", function(evento) {
+  evento.preventDefault();
+  contenedor.append(arrastrable)
+  
+});
---
+++ Examen_RA5_ARG/Ejercicio 4 ARG/js/Ejercicio 4_ARG.js
@@ -0,0 +1,38 @@
+﻿let formulario = document.getElementById("formulario");
+let input = document.getElementById("inputTexto");
+
+
+// TODO Bloquear todo lo que no sean letras o enter
+input.addEventListener("keydown", function (event) {
+  let tecla = event.code;
+
+    // Con el código no hay que distinguir entre mayúsculas y minúsculas
+    let alfabeto = ["KeyA", "KeyE", "KeyI", "KeyO", "KeyU", "KeyB", "KeyC", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "KeyM", "KeyN", "KeyÑ", "KeyP", "KeyQ", "KeyR", "KeyS", "KeyT", "KeyV", "KeyW", "KeyX", "KeyY", "KeyX"];
+    
+    if (!alfabeto.includes(tecla) || event.key === 'Enter') {
+        event.preventDefault();
+        return;
+    }
+});e
+
+
+// TODO Añadir tipo de evento y validación
+input.addEventListener("input", function () {
+  let inputValue = input.value;  
+  console.log(inputValue)
+  if (inputValue.length > 10) {
+    input.value = inputValue.slice(0, 10)
+    // console.log(inputValue.slice(0, 10))
+  } 
+});
+
+// Impedir envío si hay menos de 6 caracteres
+formulario.addEventListener("submit", function (evento) {
+  let inputValue = input.value;  
+  // TODO Código para impedir envío  
+  if (inputValue.length < 6) {
+    evento.preventDefault();
+    alert("Tiene que haber un mínimo de carácteres en el input para poder enviar al formulario.")
+  }
+
+});
